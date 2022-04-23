fee1f623f01e35277bcb4634605ce349c93d0500
sushmoyr <sushmoyr@gmail.com>
2022-04-23 17:01:23 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:window_manager/window_manager.dart';
+import "dart:io";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows) {
+    await windowManager.ensureInitialized();
+
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitle('News App');
+      await windowManager.setTitleBarStyle(TitleBarStyle.normal);
+      await windowManager.setBackgroundColor(Colors.transparent);
+      await windowManager.setSize(const Size(755, 545));
+      await windowManager.setMinimumSize(const Size(755, 545));
+      await windowManager.center();
+      await windowManager.show();
+      await windowManager.setSkipTaskbar(false);
+      await windowManager.setPreventClose(true);
+    });
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FluentApp(
+      title: 'News app',
+      theme: ThemeData(
+        accentColor: Colors.orange,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        accentColor: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  final viewKey = GlobalKey();
+  int _index = 0;
+
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      key: viewKey,
+      pane: NavigationPane(
+        selected: _index,
+        onChanged: (index) => setState(() {
+          _index = index;
+        }),
+        displayMode: PaneDisplayMode.compact,
+        items: [
+          PaneItem(
+            icon: const Icon(FluentIcons.news),
+            title: const Text('Top Headlines'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.business_center_logo),
+            title: const Text('Business'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.laptop_secure),
+            title: const Text('Technology'),
+          ),
+        ],
+      ),
+      content: NavigationBody.builder(
+        index: _index,
+        itemBuilder: (ctx, i) {
+          return Text('index: $_index');
+        },
+      ),
+    );
+  }
+
+  @override
+  void onWindowClose() async {
+    bool _isPreventClose = await windowManager.isPreventClose();
+    if (_isPreventClose) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return ContentDialog(
+            title: const Text('Confirm Close'),
+            content: const Text('Are you sure want to close the window?'),
+            actions: [
+              FilledButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  windowManager.destroy();
+                },
+              ),
+              FilledButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
---
+++ lib/models/news_page.dart
@@ -0,0 +1,6 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class NewsPage {
+  final String title;
+  final IconData iconData;
+}
